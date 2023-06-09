5c5b2f1e2145e78eb9ef36a4c5efff0f68b6a57f
NxtPerfect <alakaxan@protonmail.com>
2023-06-09 11:16:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''Config in a file
+read wallpaper-dark
+wallpaper-light
+theme-dark
+theme-light
+cursor-dark
+cursor-light
+change wallpaper with feh
+figure out how to change themes
+figure out how to change cursor'''
+
+if __name__ == "__main__":
+    pass
