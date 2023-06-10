40ccaee3350aaca5783538ca4671079fecc3e5bc
NxtPerfect <alakaxan@protonmail.com>
2023-06-10 18:49:16 +0200

    Finished config module for this stage, can read and write from config.toml file

---
+++ config.py
@@ -0,0 +1,56 @@
+import toml
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+
+def read_themes_configuration(file):
+    try:
+        with open(file, 'rb') as configfile:
+            try:
+                data = tomllib.load(configfile)
+            except tomllib.TOMLDecodeError as e:
+                print(f'failed, {e}')
+                return -1
+            print(f'{data["runtime"]}')
+
+            return data['light_theme'] if data['runtime']['current_theme'] == 'light_theme' else data['dark_theme']
+    except FileNotFoundError:
+        return -1  # Couldn't read file
+
+
+def read_current_theme(file):
+    try:
+        with open(file, 'rb') as configfile:
+            try:
+                data = tomllib.load(configfile)
+            except tomllib.TOMLDecodeError as e:
+                print(f'failed, {e}')
+                return -1
+            return data['runtime']['current_theme']
+    except FileNotFoundError:
+        return -1
+
+
+def write_current_theme(file, theme):
+    if theme == 'dark_theme':
+        theme = 'light_theme'
+    else:
+        theme = 'dark_theme'
+    print(theme)
+
+    try:
+        with open(file, 'rb') as configfile:
+            data = tomllib.load(configfile)
+            data['runtime']['current_theme'] = theme
+            print('Success!')
+    except FileNotFoundError:
+        return -1  # Couldn't read file
+    try:
+        with open(file, 'w') as configfile:
+            print(data)
+            toml.dump(data, configfile)
+
+    except FileNotFoundError:
+        return -2  # Couldn't write to file
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import config
+
 '''Config in a file
 read wallpaper-dark
 wallpaper-light
@@ -9,5 +11,26 @@ change wallpaper with feh
 figure out how to change themes
 figure out how to change cursor'''
 
+'''We should handle errors either in main or another module
+do not write errors already in the modules itself, there\'s no need'''
+
+DEFAULT_PATH = '$XDG_CONFIG_HOME/theme-switcher/config.toml'
+
+
+def run():
+    theme = 'yes'
+    try:
+        theme = config.read_themes_configuration('./config.toml')
+    except:
+        print('Couldn\'t find config file')
+    print(f'Config read {theme}')
+
+    try:
+        config.write_current_theme(
+            './config.toml', config.read_current_theme('./config.toml'))
+    except:
+        print('Couldn\'t write to file')
+
+
 if __name__ == "__main__":
-    pass
+    run()
